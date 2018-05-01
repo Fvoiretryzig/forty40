6c64ff2d7aa18badf425cae5a5382409cb264ba6
tracer <tracer@tracer.org>
2018-05-01 10:49:45 +0800

    > build forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -24,7 +24,8 @@ static void os_run() {
 
 static _RegSet *os_interrupt(_Event ev, _RegSet *regs) {
   if (ev.event == _EVENT_IRQ_TIMER){
-  	printf("this is timer??\n");
+  	//printf("this is timer??\n");
+  	printf("this is reg:0x%x\n", reg->ebp);
   }
   //_putc('*');
   if (ev.event == _EVENT_IRQ_IODEV) _putc('I');
