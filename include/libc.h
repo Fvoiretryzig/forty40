84f8b7dfbd73df864de28092a1253937a92e03a3
tracer <tracer@tracer.org>
2018-05-01 09:27:13 +0800

    > build forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

---
+++ include/libc.h
@@ -0,0 +1,40 @@
+#ifndef __LIBC_H__
+#define __LIBC_H__
+
+#include <stdint.h>
+#include <stddef.h>
+#include <os.h>
+
+// string.h
+void *memset(void *b, int c, size_t n);
+void *memcpy(void *dst, const void *src, size_t n);
+size_t strlen(const char* s);
+char *strcpy(char *dst, const char *src);
+char *strncpy(char *dst, const char *src, size_t n);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
+
+// stdlib.h
+char *itoa(int n);
+void srand(unsigned int seed);
+int rand();
+
+// stdio.h
+int printf(const char* fmt, ...);
+int sprintf(char* out, const char* format, ...);
+int snprintf(char* s, size_t n, const char* format, ...);
+
+// assert.h
+#ifdef NDEBUG
+  #define assert(ignore) ((void)0)
+#else
+  #define assert(cond) \
+    do { \
+      if (!(cond)) { \
+        printf("Assertion fail at %s:%d\n", __FILE__, __LINE__); \
+        _halt(1); \
+      } \
+    } while (0)
+#endif
+
+#endif
---
+++ src/libc/string.c
@@ -0,0 +1,81 @@
+#include<libc.h>
+
+void *memset(void *b, int c, size_t n)
+{
+	if(b == NULL || n < 0){
+		printf("null in memset\n");
+		return NULL;
+	}
+	char *temp_b = (char *)b;
+	while(n--)
+		*temp_b++ = c;
+	return b;
+}
+void *memcpy(void *dst, const void *src, size_t n)
+{
+	if(dst == NULL || src == NULL){
+		printf("null in memcpy\n");
+		return NULL;
+	}
+	char *temp_src = (char *)src;
+	char *temp_dst = (char *)dst;
+	if(((temp_src <= temp_dst) && (temp_dst <temp_src+n)) ||
+	   ((temp_src >= temp_dst) && (temp_src <temp_src+n))){
+		printf("this is overlap in memcpy");//printf("this is no solution\n");
+		return NULL;
+	}
+    while(n--){
+        *temp_dst = *temp_src;
+        temp_dst++;
+        temp_src++;
+    }
+    return dst;			
+}
+size_t strlen(const char* s)
+{
+	const char *temp = s;
+	while(*temp++);
+	return (temp-s-1);
+}
+char *strcpy(char *dst, const char *src)
+{
+	char *temp_dst = dst;
+	//while((*temp_dst++ = *src++));
+	while((*temp_dst++ = *src++)!='\0');
+	return dst;
+}
+char *strncpy(char *dst, const char *src, size_t n)
+{
+	char *temp_dst = dst;
+	while(n-- && (*temp_dst++ = *src++));
+	if(n)
+		while(--n)
+			*temp_dst = '\0';
+	return dst;
+}
+int strcmp ( const char* s1, const char* s2 )  
+{  
+    int ret = 0 ;  
+    while(*s1 == *s2 && *s2){
+    	s1++; s2++;
+    }
+   	int tmp = *s1 -*s2;
+   	if(tmp > 0)
+   		ret = 1;
+   	else if(tmp < 0)
+   		ret = -1;
+    return ret;  
+}  
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+	int ret = 0;
+	while(*s1 == *s2 && *s2 && n--){
+		s1++; s2++;
+	}
+	int temp = *s1 - *s2;
+    if(temp > 0)
+    	ret = 1;
+    else if(temp < 0)
+    	ret = -1; 
+    return ret;  		
+}
