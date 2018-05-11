2c81fb5f7b4980a1812ea14946e72286e41ddb14
tracer <tracer@tracer.org>
2018-05-11 09:32:45 +0800

    > build forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- include/unistd.h
+++ include/unistd.h
@@ -2,9 +2,9 @@
 #define __UNISTD_H__
 
 #include <os.h>
-typedef int *intptr_t;
 
-extern intptr_t program_break;
+
+extern int* program_break;
 
 int brk(void *addr);
 void *sbrk(intptr_t increment);
