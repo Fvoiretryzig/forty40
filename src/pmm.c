cf3f9a7bb676933b21a9be3259ced39fbd63a75e
tracer <tracer@tracer.org>
2018-05-11 09:48:35 +0800

    > build forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/pmm.c
+++ src/pmm.c
@@ -137,9 +137,9 @@ static void pmm_init();
 static void* pmm_alloc(size_t size);
 static void pmm_free(void *ptr); 
 MOD_DEF(pmm){
-	.init = pmm_init(),
-	.alloc = pmm_alloc(size_t size),
-	.free = pmm_free(void *ptr),
+	.init = pmm_init,
+	.alloc = pmm_alloc,
+	.free = pmm_free,
 };
 static void pmm_init()
 {
