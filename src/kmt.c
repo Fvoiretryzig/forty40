d544a0bf59b04ae3f1f0bab5a2ba6ac6b9f207ba
tracer <tracer@tracer.org>
2018-05-11 10:12:55 +0800

    > build forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -1,18 +1,4 @@
 #include <os.h>
 
-/*typedef struct thread thread_t;
-typedef struct spinlock spinlock_t;
-typedef struct semaphore sem_t;*/
-
-MODULE {
-//  void (*init)();
-//  int (*create)(thread_t *thread, void (*entry)(void *arg), void *arg);
-//  void (*teardown)(thread_t *thread);
-//  thread_t *(*schedule)();
-//  void (*spin_init)(spinlock_t *lk, const char *name);
-//  void (*spin_lock)(spinlock_t *lk);
-//  void (*spin_unlock)(spinlock_t *lk);
-//  void (*sem_init)(sem_t *sem, const char *name, int value);
-//  void (*sem_wait)(sem_t *sem);
-//  void (*sem_signal)(sem_t *sem);
-} MOD_NAME(kmt);
+MOD_DEF(kmt) {
+};
