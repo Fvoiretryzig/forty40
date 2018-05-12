8ea11b62407b298cbd1205988f7935c45211ae0e
tracer <tracer@tracer.org>
2018-05-12 09:39:47 +0800

    > build forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -1,19 +1,19 @@
 #include <os.h>
 #include <libc.h>
 
-
-
 static void os_init();
 static void os_run();
 static _RegSet *os_interrupt(_Event ev, _RegSet *regs);
 
-MOD_DEF(os) {
-  .init = os_init,
-  .run = os_run,
-  .interrupt = os_interrupt,
+MOD_DEF(os) 
+{
+	.init = os_init,
+	.run = os_run,
+	.interrupt = os_interrupt,
 };
 
-static void os_init() {
+static void os_init() 
+{
   //for (const char *p = "Hello, OS World!\n"; *p; p++) {
   //  _putc(*p);
   //}
@@ -27,14 +27,25 @@ static void os_run() {
 }
 
 static _RegSet *os_interrupt(_Event ev, _RegSet *regs) {
-  if (ev.event == _EVENT_IRQ_TIMER){
-	//printf("this is irq_timer\n");
-  }
-  if (ev.event == _EVENT_IRQ_IODEV) _putc('I');
-  if (ev.event == _EVENT_ERROR) {
-    _putc('x');
-    _halt(1);
-  }
+	if(ev.event == _EVENT_IRQ_TIMER){
+		//printf("this is irq_timer\n");
+		int _asye_init(_RegSet *(*handler)(ev, *regs));
+	}//时钟中断；
+	if(ev.event == _EVENT_IRQ_IODEV) 
+		_putc('I');	//设备中断；
+	if(ev.event == _EVENT_ERROR) {
+		_putc('x');
+		_halt(1);
+	}//其他异常；
+	if(ev.event == _EVENT_PAGEFAULT)
+		printf("this is pagefault...\n");
+	if(ev.event == _EVENT_YIELD)
+		printf("request trap into kernal...\n");
+	if(ev.event == _EVENT_SYSCALL)
+		printf("call system???????\n");
+	
+/*_EVENT_PAGEFAULT: 缺页异常，其中ev.cause存放异常发生的原因(_PROT_NONE: 页未被映射;_PROT_READ: 读取时出错; _PROT_WRITE: 写入时出错; _PROT_EXEC: 执行时出错)，ev.ref存放访问的地址。*/
+
   //return NULL; // this is allowed by AM
   return regs;
 }
