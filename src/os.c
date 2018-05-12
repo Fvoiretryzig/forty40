37e28d7a96688f47b7474d08c15ea8b1aa83d8eb
tracer <tracer@tracer.org>
2018-05-12 15:58:06 +0800

    > build forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -73,6 +73,7 @@ static _RegSet *os_interrupt(_Event ev, _RegSet *regs) {
 		
 	if(ev.event == _EVENT_YIELD){
 		printf("request trap into kernal...\n");
+		asm volatile("int $0x81"); 
 	}
 		
 	if(ev.event == _EVENT_SYSCALL){
