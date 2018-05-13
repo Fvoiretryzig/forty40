7e01768d1ee5ea2d25fcec15301352f8ecf7e8e7
tracer <tracer@tracer.org>
2018-05-14 00:33:33 +0800

    > run forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -178,7 +178,7 @@ static void sem_wait(sem_t *sem)
 		sem->queue[i] = 1;
 		while(sem->queue[i]);
 	}
-	//printf("/*=====in kmt.c 188line sem_wait()====*/\nsem->name:%s sem->count:%d\n", sem->name, sem->count);
+	printf("/*=====in kmt.c 188line sem_wait()====*/\nsem->name:%s sem->count:%d\n", sem->name, sem->count);
 	return;
 }
 static void sem_signal(sem_t *sem)
@@ -194,7 +194,7 @@ static void sem_signal(sem_t *sem)
 		}
 		sem->queue[i] = 0;
 	}
-	//printf("/*=====in kmt.c 203line sem_signal()====*/\nsem->name:%s sem->count:%d\n", sem->name, sem->count);
+	printf("/*=====in kmt.c 203line sem_signal()====*/\nsem->name:%s sem->count:%d\n", sem->name, sem->count);
 	return;
 }
 /*#include <os.h>
