651799a2afbfa57ca66aa8f354779e8383ed9e2f
tracer <tracer@tracer.org>
2018-05-16 02:02:21 +0800

    > run forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -234,7 +234,7 @@ static void sem_signal(sem_t *sem)
 		sem->queue[i] = 0;
 		//if(work_head->next)
 		//	_yield();
-		printf("in signal 202 sem->name:%s queue: 0:%d 1:%d count:%d\n", sem->name, sem->queue[0], sem->queue[1],sem->count);
+		//printf("in signal 202 sem->name:%s queue: 0:%d 1:%d count:%d\n", sem->name, sem->queue[0], sem->queue[1],sem->count);
 		//printf("name:%s sem->queue[i] = 0 i:%d\n", sem->name,i);
 	}
 	spin_unlock(&sem_lk);
