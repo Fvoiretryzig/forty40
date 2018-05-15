71b22931c0dc3a95fb26d46c9cc19c0fd8e3fbe2
tracer <tracer@tracer.org>
2018-05-16 00:44:02 +0800

    > run forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -83,8 +83,8 @@ static int create(thread_t *thread, void (*entry)(void *arg), void *arg)
 		work_head = current;
 		
 		printf("/*=====in kmt.c 78line create()====*/\ntid:%d current:0x%08x current->next:0x%08x current->t->id:%d\n\n", thread->id, current, current->next, current->t->id);
-		printf("/*=====in kmt.c 80line create()====*/\nwork_head:0x%08x work_head->next:0x%08x\n",
-			work_head, work_head->next);		
+		printf("/*=====in kmt.c 80line create()====*/\nwork_head:0x%08x work_head->next:0x%08x work_head->next id:%d\n",
+			work_head, work_head->next,work_head->next->t->id);		
 		current = work_head;
 		//if(current->next)
 		//	printf("current->next->t->id:%d\n",current->next->t->id);
