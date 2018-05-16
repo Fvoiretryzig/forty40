db9a7b80b8bdea23a312a6732473897356bfe3e9
tracer <tracer@tracer.org>
2018-05-17 00:34:55 +0800

    > run forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -112,7 +112,7 @@ static void teardown(thread_t *thread)
 }
 static thread_t* schedule()
 {		
-	printf("this is in schedule!!!!\n");
+	printf("\nthis is in schedule!!!!\n");
 	struct thread_node* current = pmm->alloc(sizeof(struct thread_node));
 	//printf("kmt115\n");
 	current = work_head;
@@ -189,7 +189,7 @@ static void sem_init(sem_t *sem, const char *name, int value)
 static void sem_wait(sem_t *sem)
 {
 	spin_lock(&sem_lk);
-	printf("this is in sem_wait!!!!\n");
+	printf("\nthis is in sem_wait!!!!\n");
 	sem->count--;
 	//printf("name:%s sem->count--;\ncount:%d\n", sem->name, sem->count);
 	//printf("/*=====in kmt.c 128line sem_wait()====*/sem->name:%s count:%d\n", sem->name,sem->count);
@@ -241,7 +241,7 @@ static void sem_wait(sem_t *sem)
 static void sem_signal(sem_t *sem)
 {
 	spin_lock(&sem_lk);
-	printf("this is in sem_signal!!!!!\n");
+	printf("\nthis is in sem_signal!!!!!\n");
 	sem->count++;
 	//printf("name:%s sem->count++;\ncount:%d\n", sem->name, sem->count);
 	//printf("/*=====in kmt.c 128line sem_signal()====*/sem->name:%s\n", sem->name);
