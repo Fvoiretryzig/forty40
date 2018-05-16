64d83241e4d6121dc277cc9dc0e7f913c6fd09d5
tracer <tracer@tracer.org>
2018-05-17 00:22:21 +0800

    > run forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -162,7 +162,7 @@ static void spin_lock(spinlock_t *lk)
 	//printf("%s in lock intr:%d\n", lk->name, _intr_read());
 	if(_intr_read())
 		_intr_write(0);		//关中断
-	//printf("intr_read:%d\n", _intr_read());
+	printf("%s in lock intr:%d\n",lk->name,  _intr_read());
 	while(_atomic_xchg(&lk->locked, 1) != 0);
 }
 static void spin_unlock(spinlock_t *lk)
