4071ac4739fea11c45f9c0b1e9371302efe7f4c0
tracer <tracer@tracer.org>
2018-05-17 01:12:58 +0800

    > run forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -2,7 +2,7 @@
 #include <libc.h>
 
 sem_t empty, fill;
-thread_t t1, t2, t3;//,t4,t5, t6, t7, t8, t9, t10;
+thread_t t1, t2, t3,t4;//,t5, t6, t7, t8, t9, t10;
 #define BUF_SIZE 4
 
 static void producer() {
@@ -39,6 +39,7 @@ static void test_run() {
   	kmt->create(&t2, &consumer, NULL);
   	//printf("before create t3\n");
   	kmt->create(&t3, &producer, NULL);	
+  	kmt->create(&t4, &producer, NULL);
   	kmt->spin_unlock(&lk);
   	//printf("in test run _intr_read():%d\n",_intr_read());
 /*	printf("before create t1\n");
