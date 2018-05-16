091101ebe2af6b2dce3bd6884b107033bc9a62ac
tracer <tracer@tracer.org>
2018-05-16 13:30:10 +0800

    > build forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -28,16 +28,19 @@ static void consumer() {
 	}
 }
 //thread_t t1; thread_t t2;
-
+spinlock_t lk;
 static void test_run() {
+	kmt->spin_init(&lk,"intestrun");
 	kmt->sem_init(&empty, "empty", BUF_SIZE);
 	kmt->sem_init(&fill, "fill", 0);
+	spin_lock(&lk);
   	printf("before create t1\n");
   	kmt->create(&t1, &producer, NULL);
   	printf("before create t2\n");
   	kmt->create(&t2, &producer, NULL);
   	printf("before create t3\n");
   	kmt->create(&t3, &consumer, NULL);	
+  	spin_unlock(&lk);
 /*	printf("before create t1\n");
   	kmt->create(&t1, &producer, NULL);
   	printf("before create t2\n");
