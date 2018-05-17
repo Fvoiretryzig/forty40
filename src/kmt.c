27eaa080da534b45642af1d9cc2a5144dc76f307
tracer <tracer@tracer.org>
2018-05-17 23:46:50 +0800

    > build forty Linux forty-virtual-machine 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 i686 i686 i686 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -21,7 +21,7 @@ static void sem_init(sem_t *sem, const char *name, int value);
 static void sem_wait(sem_t *sem);
 static void sem_signal(sem_t *sem);
 thread_t* work[T_max];
-extern int thread_num;
+int thread_num;
 int point;
 MOD_DEF(kmt) 
 {
