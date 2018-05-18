bef29db019ff524599d62c7054cf5993ecb74b87
tracer <tracer@tracer.org>
2018-05-18 10:07:55 +0800

    > run forty Linux forty-virtual-machine 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -196,8 +196,9 @@ static void sem_wait(sem_t *sem)
 		}
 		//!@#$printf("kmt wait 221 last_node:0x%08x last_node->if_in:%d\n",last_node, last_node->if_in);
 		spin_unlock(&sem_lk);
-		while(last_node->if_in);
-			//!@#$printf("last_node:0x%08x\n", last_node);
+		while(last_node->if_in){
+			printf("last_node:0x%08x\n", last_node);
+		}
 		spin_lock(&sem_lk);
 		sem->count--;
 		if(last_node->prev || last_node->next){
