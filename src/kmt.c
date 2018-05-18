0837a1bd0e002774548f23e3390d53d1541ce1f5
tracer <tracer@tracer.org>
2018-05-18 09:21:26 +0800

    > run forty Linux forty-virtual-machine 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- src/kmt.c
+++ src/kmt.c
@@ -112,7 +112,7 @@ static thread_t* schedule()
 	int old_id = current_id;
 	printf("id:%d ", work[old_id].id);
 	printf("thread stack:0x%08x ", work[old_id].stack);
-	printf("eip 0x%08x:0x%08x\n",work[old_id].thread_reg->eip, work[old_id].thread_reg->eip);
+	printf("eip 0x%08x:0x%08x\n",&work[old_id].thread_reg->eip, work[old_id].thread_reg->eip);
 	current_id = (current_id+1)%thread_cnt;
 	
 	printf("\n");
