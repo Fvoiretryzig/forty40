49ca1b740810aa3bd22abf30dfb81d00e8d0eca5
tracer <tracer@tracer.org>
2018-05-19 15:13:55 +0800

    > build forty Linux forty-virtual-machine 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- include/test.h
+++ include/test.h
@@ -7,10 +7,6 @@ sem_t empty, fill;
 thread_t t1, t2,t3,t4,t5,t6,t7, t8 ,t9, t10,t11,t12,t13,t14,t15,t16;
 #define BUF_SIZE 4
 
-thread_t work[T_max];
-int thread_cnt;
-int current_id;
-int last_thread;
 
 void producer();
 void consumer();
