3bb78d989716b98941170d88dabde14a3dd85518
tracer <tracer@tracer.org>
2018-05-19 15:22:46 +0800

    > build forty Linux forty-virtual-machine 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:03 UTC 2018 i686 i686 i686 GNU/Linux

--- include/test.h
+++ include/test.h
@@ -1,11 +1,11 @@
 #ifndef __TEST_H__
-#define __TEST_H__
+//#define __TEST_H__
 
 #include <kernel.h>
 #include <libc.h>
 
 sem_t empty, fill;
-thread_t t1, t2,t3,t4,t5,t6,t7, t8 ,t9, t10,t11,t12,t13,t14,t15,t16;
+thread_t t1, t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16;
 #define BUF_SIZE 4
 
 
