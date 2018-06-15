6bfc6ed84a0f155b402da2d8a37a9b12ed871fd9
tracer <tracer@tracer.org>
2018-06-15 21:59:45 +0800

    > build forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- include/vfs.h
+++ include/vfs.h
@@ -11,12 +11,18 @@
 //#define SEEK_END 2
 
 #define file_content_maxn 1024
-#define file_cnt 512
 #define inode_cnt 512
 #define name_len 128
 #define fd_cnt 2048
 #define file_cnt 2048
-typedef struct inode
+
+typedef struct inode inode_t;
+typedef struct fileops fileops_t;
+typedef struct file file_t;
+typedef struct fsops fsops_t;
+typedef struct filesystem filesystem_t;
+typedef struct mount_path mount_path_t;
+struct inode
 {
 	//int inode_no;
 	int if_write;
@@ -25,16 +31,16 @@ typedef struct inode
 	char name[name_len];
 	char content[file_content_maxn];
 	int size;	//文件大小
-}inode_t;
-typedef struct fileops 
+};
+struct fileops 
 {
 	int (*open)(inode_t *inode, file_t *file, int flags);
 	ssize_t (*read)(inode_t *inode, file_t *file, char *buf, size_t size);
 	ssize_t (*write)(inode_t *inode, file_t *file, const char *buf, size_t size);
 	off_t (*lseek)(inode_t *inode, file_t *file, off_t offset, int whence);
 	int (*close)(inode_t *inode, file_t *file);
-} fileops_t;
-typedef struct file
+};
+struct file
 {
 	int if_read;
 	int if_write;
@@ -44,25 +50,24 @@ typedef struct file
 	char name[name_len];
 	int fd;
 	fileops_t ops;
-}file_t;
-typedef struct fsops 
+};
+struct fsops 
 {
 	void (*init)(struct filesystem *fs, const char *name, inode_t *dev);
 	inode_t *(*lookup)(struct filesystem *fs, const char *path, int flags);
 	int (*close)(inode_t *inode);
-} fsops_t;
-typedef struct filesystem
+};
+struct filesystem
 {
 	char name[name_len];
 	//char mount_path[name_len];
 	mountpath_t* path;
 	inode_t *inode[inode_cnt];
-	//file_t *file[file_cnt];
 	fsops_t ops;
-}filesystem_t;
-typedef struct mount_path
+};
+struct mount_path
 {
 	char p[64];
 	filesystem_t* fs;
-}mountpath_t;
+};
 #endif
