0361d9eb6f0f22d4f085f3bb66e1da6f4227604a
tracer <tracer@tracer.org>
2018-06-20 00:54:30 +0800

    > build forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- include/test.h
+++ include/test.h
@@ -3,6 +3,7 @@
 
 #include <kernel.h>
 #include <libc.h>
+#include <vfs.h>
 
 sem_t empty, fill;
 thread_t t1, t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16;
@@ -13,5 +14,7 @@ void producer();
 void consumer();
 spinlock_t lk;
 void test_run();
+
+void dev_test()
  
 #endif
