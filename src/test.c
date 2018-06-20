b23c5987a519a0bb0923d14ee868227112e2c280
tracer <tracer@tracer.org>
2018-06-20 23:43:36 +0800

    > run forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- src/test.c
+++ src/test.c
@@ -52,61 +52,61 @@ void dev_test()
 	char *buf = pmm->alloc(1024); int size = 0;
 	/*========================random========================*/
 	int random_fd = vfs->open("/dev/random", O_RDONLY);
-	printf("random fd:%d\n", random_fd);
+	printf("dev:random fd:%d\n", random_fd);
 	size = vfs->read(random_fd, buf, 0);
 	if(size < 0){
-		printf("error read /dev/random in dev_test\n");
+		printf("dev:error read /dev/random in dev_test\n");
 		vfs->close(random_fd);
 		pmm->free(buf);
 		return;
 	}
-	printf("this is the random number return by /dev/random:%s size:%d\n", buf, size);
+	printf("dev:this is the random number return by /dev/random:%s size:%d\n", buf, size);
 	size = vfs->read(random_fd, buf, 0);
 	if(size < 0){
-		printf("error read /dev/random in dev_test\n");
+		printf("dev:error read /dev/random in dev_test\n");
 		vfs->close(random_fd);
 		pmm->free(buf);
 		return;
 	}
-	printf("this is the random number return by /dev/random:%s size:%d\n", buf, size);
+	printf("dev:this is the random number return by /dev/random:%s size:%d\n", buf, size);
 	/*========================null========================*/
 	int null_fd = vfs->open("/dev/null", O_RDWR);
-	printf("null fd:%d\n", null_fd);
+	printf("dev:null fd:%d\n", null_fd);
 	strcpy(buf, "40404040");
 	size = vfs->write(null_fd, buf, strlen(buf));
 	if(size < 0){
-		printf("error write /dev/null\n");
+		printf("dev:error write /dev/null\n");
 		vfs->close(null_fd);
 		pmm->free(buf);
 		return;
 	}
-	printf("this is the writing /dev/null operation return value:%d\n", size);
+	printf("dev:this is the writing /dev/null operation return value:%d\n", size);
 	size = vfs->read(null_fd, buf, 0);
 	if(size < 0){
-		printf("error read /dev/null\n");
+		printf("dev:error read /dev/null\n");
 		vfs->close(null_fd);
 		pmm->free(buf);
 		return;
 	}
-	printf("after read /dev/null buf:%d\n", *buf);
+	printf("dev:after read /dev/null buf:%d\n", *buf);
 	vfs->close(null_fd);
 	/*========================zero========================*/
 	int zero_fd = vfs->open("/dev/zero", O_RDONLY);
-	printf("zero fd:%d\n", zero_fd);
+	printf("dev:zero fd:%d\n", zero_fd);
 	strcpy(buf, "40404040");
-	printf("this is buf in zero before read buf:%s\n", buf);
+	printf("dev:this is buf in zero before read buf:%s\n", buf);
 	size = vfs->read(zero_fd, buf, 0);
 	if(size < 0){
-		printf("error read /dev/zero\n");
+		printf("dev:error read /dev/zero\n");
 		vfs->close(zero_fd);
 		pmm->free(buf);
 		return;
 	}	
-	printf("after read /dev/zero buf:%d\n", *buf);
+	printf("dev:after read /dev/zero buf:%d\n", *buf);
 	vfs->close(zero_fd);
 	vfs->close(random_fd);
 	pmm->free(buf);
-	printf("this is checkpoint\n");
+	printf("dev:this is checkpoint\n");
 	return;
 }
 void proc_test()
@@ -119,36 +119,36 @@ void kv_test()
 	char *name = pmm->alloc(128);
 	strcpy(name, "/forty/40c");
 	int fd = vfs->open(name, O_CREATE|O_RDWR);
-	printf("fd for %s:%d\n", name, fd);
+	printf("kv:fd for %s:%d\n", name, fd);
 	if(fd < 0){
-		printf("open %s error!!\n", name);
+		printf("kv:open %s error!!\n", name);
 		return;
 	}
 	strcpy(buf, "forty-forty\nthis is a test for kvdb\n40404040\n");
 	size = vfs->write(fd, buf, strlen(buf));
 	if(size < 0){
-		printf("write %s error!!\n", name);
+		printf("kv:write %s error!!\n", name);
 		vfs->close(fd);
 		pmm->free(buf); pmm->free(name);
 		return;
 	}
-	printf("write %s size:%d\n", name, size);
+	printf("kv:write %s size:%d\n", name, size);
 	vfs->close(fd);
 	fd = vfs->open(name, O_RDWR);
-	printf("fd for %s:%d\n", name, fd);
+	printf("kv:fd for %s:%d\n", name, fd);
 	if(fd < 0){
-		printf("open %s error!!\n", name);
+		printf("kv:open %s error!!\n", name);
 		return;
 	}	
 	strcpy(buf, " ");
 	size = vfs->read(fd, buf, 128);
 	if(size < 0){
-		printf("read %s error!!\n", name);
+		printf("kv:read %s error!!\n", name);
 		vfs->close(fd);
 		pmm->free(buf); pmm->free(name);		
 		return;
 	}
-	printf("read %s size:%d\nread content:\n%s", name, size, buf);
+	printf("kv:read %s size:%d\nread content:\n%s", name, size, buf);
 	
 	return;
 }
