7b43bf627362b0282447c9a75d802b25c41ecd1c
tracer <tracer@tracer.org>
2018-06-20 19:03:49 +0800

    > run forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- src/vfs.c
+++ src/vfs.c
@@ -465,7 +465,7 @@ ssize_t dev_file_read(inode_t *inode, file_t *file, char*buf, size_t size)
 		buf = (char *)0;
 	}
 	else if(!strcmp(inode->name+strlen(devfs_p->p), "/null")){;
-		buf = (char *)0;
+		strcpy(buf, (char*)0);
 	}
 	else if(!strcmp(inode->name+strlen(devfs_p->p), "/random")){	
 		int num = rand() % 8192;
