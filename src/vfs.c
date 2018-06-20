dd5a085048266f7285beeedaac3420364ea34148
tracer <tracer@tracer.org>
2018-06-20 20:47:38 +0800

    > run forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- src/vfs.c
+++ src/vfs.c
@@ -467,8 +467,7 @@ ssize_t dev_file_read(inode_t *inode, file_t *file, char*buf, size_t size)
 		printf("buf in zero:%s\n", buf);
 	}
 	else if(!strcmp(inode->name+strlen(devfs_p->p), "/null")){
-		char *temp = 0;
-		*buf = *temp;
+		buf = "";
 		//strcpy(buf, temp);
 		printf("buf in null:%s\n", buf);
 		//strcpy(buf, NULL);
