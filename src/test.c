4b16f73c7991965fc9281f86e1f78609a1fa270e
tracer <tracer@tracer.org>
2018-06-21 23:55:38 +0800

    > run forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- src/test.c
+++ src/test.c
@@ -218,9 +218,9 @@ void test_file()
 	//kmt->spin_init(&lk,"test_file_lk");
 	//kmt->spin_lock(&lk);
 	
-	//kmt->create(&t1, &dev_test, NULL);
-	//kmt->create(&t2, &kv_test, NULL);
-	kmt->create(&t1, &dummy, NULL);
+	kmt->create(&t1, &dev_test, NULL);
+	kmt->create(&t2, &kv_test, NULL);
+	//kmt->create(&t1, &dummy, NULL);
 	kmt->create(&t3, &proc_test, NULL);
 	
 	
