039325d0552b1f4d6fe6741d2f135b769770df34
tracer <tracer@tracer.org>
2018-06-22 22:55:38 +0800

    > build forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- src/vfs.c
+++ src/vfs.c
@@ -827,6 +827,7 @@ ssize_t write(int fd, void *buf, size_t nbyte)
 		node = devfs_p->fs->ops->lookup(devfs_p->fs, path, 0);
 	}
 	else if(!strncmp(path, kvfs_p->p, strlen(kvfs_p->p))){
+		printf("write:kvfs_p->fs->inode[0]->name:%s\n",kvfs_p->fs->inode[0]->name);
 		node = kvfs_p->fs->ops->lookup(kvfs_p->fs, path, 0);
 	}
 	if(node == NULL){
