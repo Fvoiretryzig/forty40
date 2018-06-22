a7053c58fbb7e492c76dd674f6a875d7d9da5c79
tracer <tracer@tracer.org>
2018-06-22 23:14:41 +0800

    > run forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -35,8 +35,9 @@ static void os_run() {
   test_file();
   while (1) ; // should never return
 }
-
+spinlock_t lk;
 static _RegSet *os_interrupt(_Event ev, _RegSet *regs) {
+	spin_lock(&lk);
 	if(last_thread != -1){
 		work[last_thread].thread_reg = regs;
 	}
@@ -67,5 +68,6 @@ static _RegSet *os_interrupt(_Event ev, _RegSet *regs) {
 		printf("call system...\n");
 		//__asm__ __volatile__("int $0x80"); 
 	}
+	spin_lock(&lk);
   return t->thread_reg;
 }
