9aa24e1de37153449e3448098d2e0f37058efec4
tracer <tracer@tracer.org>
2018-06-22 22:16:21 +0800

    > run forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- src/os.c
+++ src/os.c
@@ -45,7 +45,7 @@ static _RegSet *os_interrupt(_Event ev, _RegSet *regs) {
 	last_thread = t->id;
 	//printf("in os_interrupt eip:0x%08x\n", regs->eip);
 	if(ev.event == _EVENT_IRQ_TIMER){
-		printf("this is irq_timer\n");
+		printf("this is irq_timer\n\n");
 	}//时钟中断???????????；
 	if(ev.event == _EVENT_IRQ_IODEV){
 		printf("this is _EVENT_IRQ_IODEV\n");	
@@ -59,7 +59,7 @@ static _RegSet *os_interrupt(_Event ev, _RegSet *regs) {
 	}
 		
 	if(ev.event == _EVENT_YIELD){
-		printf("request trap into kernal...\n");
+		printf("request trap into kernal...\n\n");
 		
 	}
 		
