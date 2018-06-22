f9166bebd83f2246aac7bd85f499ac7b40f6ec15
tracer <tracer@tracer.org>
2018-06-23 01:09:25 +0800

    > build forty Linux forty-virtual-machine 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:16:09 UTC 2018 i686 i686 i686 GNU/Linux

--- src/unistd.c
+++ src/unistd.c
@@ -5,7 +5,8 @@ int* program_break;
 //应该无关物理地址虚拟地址的转换……吧
 int brk(void *addr)
 {
-	if((uint32_t)addr >= (uint32_t)program_break && (uint32_t)addr < (uint32_t)_heap.end){
+	//if((uint32_t)addr >= (uint32_t)program_break && (uint32_t)addr < (uint32_t)_heap.end){
+	if((uint32_t)addr >= (uin32_t)_heap.start && (uint32_t)addr < (uint32_t)_heap.end){
 		program_break = addr;
 		return 0;
 	}
